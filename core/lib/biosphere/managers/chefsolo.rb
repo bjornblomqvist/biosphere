1a54f8dcb7e7dd7455c9a2f80f4467b570a3dd84
future <commanda.keen@gmail.com>
2013-07-01 09:59:16 +0200

    Allow automatic download of custom chefsolo cookbooks in git

--- core/lib/biosphere/managers/chefsolo.rb
+++ core/lib/biosphere/managers/chefsolo.rb
@@ -1,7 +1,17 @@
+require 'uri'
 require 'biosphere/managers/chef'
 require 'biosphere/resources/gem'
 require 'biosphere/resources/command'
 require 'biosphere/resources/file'
+
+module Biosphere
+  module Errors
+    class CouldNotUpdateRemoteCookbooks < Error
+      def code() 67 end
+    end
+  end
+end
+
 
 module Biosphere
   module Managers
@@ -16,7 +26,51 @@ module Biosphere
 
       def ensure_knife_config
         super
+        ensure_cookbooks
         Resources::File.write chef_json_path, chef_json_template
+      end
+
+      def ensure_cookbooks
+        if cookbooks_path && cookbooks_path.exist?
+          update_cookbooks
+        else
+          clone_cookbooks
+        end
+      end
+
+      def clone_cookbooks
+        Log.info "Cloning remote cookbooks from #{cookbooks_repo}"
+        Log.info "Cloning into #{cookbooks_path}"
+        result = Resources::Command.run :executable => 'git', :arguments => %W{ clone #{cookbooks_repo} #{cookbooks_path} }
+      end
+
+      def update_cookbooks
+        Log.info "Updating remote cookbooks from #{cookbooks_repo}"
+        work_tree = cookbooks_path
+        git_dir = cookbooks_path.join('.git')
+        result = Resources::Command.run :executable => 'git', :arguments => %W{ --work-tree #{work_tree} --git-dir #{git_dir} pull origin master }
+        if result.success?
+          Log.info "Cookbooks were updated."
+        else
+          message = "Could not update cookbooks: #{result.stdout.strip} #{result.stderr.strip}"
+          Log.error message
+          raise Errors::CouldNotUpdateRemoteCookbooks, message
+        end
+      end
+
+      def cookbooks_repo
+        return unless config.cookbooks_repo
+        Pathname.new config.cookbooks_repo
+      end
+
+      def cookbooks_path
+        return unless cookbooks_repo
+        name = File.basename URI.parse(cookbooks_repo).path.split('/').last, '.*'
+        cookbooks_container_path.join name
+      end
+
+      def cookbooks_container_path
+        sphere.path.join('cookbooks')
       end
 
       def chef_json_template
@@ -33,8 +87,20 @@ module Biosphere
 
       def knife_config_template
         result = super
-        cookbook_path = Array(knife_config[:cookbook_path]).map { |path| File.expand_path(path) }
-        result += %{\ncookbook_path %w{ #{cookbook_path.join(' ')} }}
+        paths = Array(knife_config[:cookbooks_path])
+        if cookbooks_repo
+          if path = paths.first
+            path = cookbooks_path.join(path)
+            Log.debug "Using cookbooks located at relative path #{path}"
+            result += %{\ncookbook_path %w{ #{path} }}
+          else
+            Log.debug "Using cookbooks located at absolute path#{cookbooks_path}"
+            result += %{\ncookbook_path %w{ #{cookbooks_path} }}
+          end
+        else
+          paths = paths.map { |path| File.expand_path(path) }
+          result += %{\ncookbook_path %w{ #{paths.join(' ')} }}
+        end
       end
 
       def chef_json_path
